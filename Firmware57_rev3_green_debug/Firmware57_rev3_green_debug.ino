82cef5398b111a4add73ae62632a27efd14c10e9
childofthehorn <childofthehorn@gmail.com>
2014-04-23 20:08:07 -0500

    New Feature : Save/Clear/Load SerialBaud Rate information
            Uses the Command set to save Baud Rate to the EEPROM
    
             - 0x9E to set the Baud rate to use
             - 0x9E Save and start using desired baud rate
             - 0x9F Clear you Baud rate
    
    Signed-off-by: childofthehorn <childofthehorn@gmail.com>

--- Firmware57_rev3_green_debug/Firmware57_rev3_green_debug.ino
+++ Firmware57_rev3_green_debug/Firmware57_rev3_green_debug.ino
@@ -2,7 +2,7 @@
  * Modular Scrolling LED Matrix - LED Board Firmware
  * (C) 2011-2014 Stephen Wylie & Stacy Wylie
  *
- * REVISION 3.1
+ * REVISION 3.2
  * Standardized which CPU output pins lead to which cathodes/anodes.
  * Added the auto-addressing scheme.
  * Implemented control codes for finer firmware control.
@@ -11,6 +11,9 @@
 
 #include <EEPROM.h>
 
+#define ADDR_BOARD   0
+#define ADDR_BAUD    3
+
 int procID = 0;  //Processor ID
 int rowActive[7] = {6, 11, 9, 10, 3, 4, 13};
 int colActive[5] = {5, 7, 8, 2, 12};
@@ -18,6 +21,8 @@ unsigned long supportedBauds[16] = {9600, 14400, 19200, 28800, 38400, 57600, 768
 115200, 200000, 250000, 500000, 1000000, 9600, 9600, 9600, 9600};
 int portBmask = 0b10001;
 int portDmask = 0b10100100;
+int currentBaud = 0;
+boolean inLoop;
 
 // if colData[col] & rowNums[row] != 0, then activate pin # rowActive[row]
 // colActive stores which pin # to activate for a particular column
@@ -102,47 +107,38 @@ void runCommand(int cmd) {
       showAddrDebug();
     } else if (cmd == 0x85 && (procID == boardToAffect)) {
       // Save our address (eeprom)
-      // This will have each board saving their address to the EEPROM
-      makeTheF(); // Show the user that you are changing this board
-      int byteAddr = 0;
-      eepromClearAddr(byteAddr); 
-      eepromSave(byteAddr, procID); //save It
-      eepromRead(byteAddr); //Read the new address to be used 
+      // This will have each board save their address to the EEPROM
+      makeTheF(); // Show the user that you are activating this board
+      eepromClearAddr(ADDR_BOARD);
+      eepromSave(ADDR_BOARD, procID); //save It
+      procID = eepromRead(ADDR_BOARD); //Read the new address to be used
       showAddrDebug(); //Show the Address
     } else if (cmd == 0x86 && (procID == boardToAffect)) {
       //Clear your address
-      int byteAddr = 0;
-      eepromClearAddr(byteAddr);
+      eepromClearAddr(ADDR_BOARD);
       procID = 0; 
-    } else if (cmd == 0x87) {
-      // Clear your address
+    } else if (cmd == 0x8F) {
+      // Clear ALL OF your EEPROM
       eepromClear();
       makeTheF(); // Show that the operation completed
-    } 
+    }
     // end if cmd is in the 0x80s
   } else if (cmd < 0xA0) {
-    // Codes ranging from 0x90 - 0x9F will change the serial baud rate dynamically on all boards.
-    Serial.end();                  // Stop the serial port
-    delay(100);                    // Delay 0.1 seconds
-    // Output the raw A7 pin reading onto the test pattern in this manner:
-    // X    X    X    X    X
-    // X    o    o    o    o
-    // X 512k 256k 128k  64k
-    // X  32k  16k   8k   4k
-    // X   2k   1k  512  256 
-    // X  128   64   32   16
-    // X    8    4    2    1
-    clearPanel();
-    unsigned long newBaudRate = supportedBauds[cmd & 0xF];
-    for (int i = 19; i >= 0; i--) {
-      // Columns go from MSB to LSB as shown above, hence 4 - (i % 4)
-      // If val has the (1 << i)th bit set, then shift that "1" value up by (i / 4) + 3 places
-      // +3 because we need this to appear 3 rows higher than the bottom
-      unsigned long one = (unsigned long) 1;
-      unsigned long shift = one << i;
-      colData[4 - (i % 4)] |= ((newBaudRate & shift) == shift) << (i / 4);
+    // Various commands dealing with serial baud rate
+    if (cmd < 0x9E) {
+      //We will now set and use this baud rate
+      serialBaudReset(cmd);
+    } else if (cmd == 0x9E) {
+      //Store the Desired serial baud rate
+      eepromClearAddr(ADDR_BAUD);
+      eepromSave(ADDR_BAUD, currentBaud);
+      int baudVal = eepromRead(ADDR_BAUD);
+      serialBaudReset(baudVal);
+    } else if (cmd == 0x9F) {
+      //clear out the Serial Baud rate
+      eepromClearAddr(ADDR_BAUD); //EEPROM address of Baud
     }
-    Serial.begin(newBaudRate);   // Begin the serial at the specified baud rate
+    // end if cmd is in the 0x90s
   }
   return;
 }
@@ -176,21 +172,50 @@ void clearReads() {
   return;
 }
 
+//Serial Baud restart
+void serialBaudReset(int serVal) (
+   // Codes ranging from 0x90 - 0x9F will change the serial baud rate dynamically on all boards.
+    Serial.end();                  // Stop the serial port
+    delay(100);                    // Delay 0.1 seconds
+    // Output the raw A7 pin reading onto the test pattern in this manner:
+    // X    X    X    X    X
+    // X    o    o    o    o
+    // X 512k 256k 128k  64k
+    // X  32k  16k   8k   4k
+    // X   2k   1k  512  256 
+    // X  128   64   32   16
+    // X    8    4    2    1
+    clearPanel();
+    currentBaud = serVal;
+    unsigned long newBaudRate = supportedBauds[serVal & 0xF];
+    if (inLoop) {
+      for (int i = 19; i >= 0; i--) {
+        // Columns go from MSB to LSB as shown above, hence 4 - (i % 4)
+        // If val has the (1 << i)th bit set, then shift that "1" value up by (i / 4) + 3 places
+        // +3 because we need this to appear 3 rows higher than the bottom
+        unsigned long one = (unsigned long) 1;
+        unsigned long shift = one << i;
+        colData[4 - (i % 4)] |= ((newBaudRate & shift) == shift) << (i / 4);
+      }
+    }
+    Serial.begin(newBaudRate);   // Begin the serial at the specified baud rate
+}
+
 //Clears the Entire EEPROM
 void eepromClear() {
   //ATmega168 has 512 bytes of EEPROM, ATmega328 has 1024 
   for (int i = 0; i < 512; i++)
-  EEPROM.write(i, 0);
+    EEPROM.write(i, 0);
 }
 
 //Clears specific EEPROM address
-void eepromClearAddr(int addr) {
-  EEPROM.write(addr,0);
+void eepromClearAddr(int byteLoc) {
+  EEPROM.write(byteLoc, 0);
 }
 
-//Saves the desired value to the EEPROM
-void eepromSave(int byteLoc, int brdAddr){
-  EEPROM.write(byteLoc, brdAddr);
+//Saves the desired value (data) to the specified location in EEPROM
+void eepromSave(int byteLoc, int data){
+  EEPROM.write(byteLoc, data);
 }
 
 //Read the value at this address on the EEPROM
@@ -199,32 +224,32 @@ int eepromRead(int byteLoc) {
 }
 
 // Load the Saved Address
-void readSavedAddr() {
- int byteLoc = 0;
- int proc = eepromRead(byteLoc); 
- if (proc != 0){ 
-  procID = proc;
- } 
+void getSavedBoardAddr() {
+  int proc = eepromRead(ADDR_BOARD);
+  if (proc != 0)
+    procID = proc;
 }
 
 void setup() {
   
+  inLoop = false;
   // show power
   digitalWrite(colActive[0], HIGH);
   digitalWrite(rowActive[0], HIGH);
   digitalWrite(colActive[0], LOW);
-  // initialize serial communication:
-  Serial.begin(9600);
+  // serialBaudReset can initialize serial communication too ;)
+  serialBaudReset(eepromRead(ADDR_BAUD));
   // initialize the LED pins as outputs:
   for (int i = 1; i < 14; i++)
     pinMode(i, OUTPUT);
   // Output a F-like shape to show all rows & columns are working
   makeTheF();
-  readSavedAddr();       // Read any saved address 
+  getSavedBoardAddr();       // Read any saved address
   activeProc = false;    // this processor isn't active now
   delay(1000);           // stall for 1 second
   // initialize all the readings to 0: 
   clearReads();
+  inLoop = true;
 
 }
 
